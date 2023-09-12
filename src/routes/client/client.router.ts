3f281b2ed649274a00f13a4be718b5283fbb7b31
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-09-11 23:14:26 -0400

    fix: attached paypal routes to client router

--- src/routes/client/client.router.ts
+++ src/routes/client/client.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes'
 import roomsRoutes from './rooms.routes'
+import paypalRoutes from './paypal.routes'
 
 const clientRouter = Router()
 
 clientRouter.use('/', authRoutes)
 clientRouter.use('/rooms', roomsRoutes)
+clientRouter.use('/paypal', paypalRoutes)
 
 export default clientRouter
